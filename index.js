0b2bf371eaeffeba9d21191a22fdb8f5bb98ab7d
Aurimas <aurimas@darguzis.com>
2019-06-11 10:39:14 +0100

    db is up and running

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-import { getHTML, getInstagramCount, getTwitterCount } from './lib/scraper';
+import express from 'express';
+import { getInstagramCount, getTwitterCount } from './lib/scraper';
+import db from './lib/db';
 
-async function go() {
+const app = express();
+
+app.get('/scrape', async (req, res, next) => {
+  console.log('Scraping!');
   const [iCount, tCount] = await Promise.all([
     getInstagramCount(),
     getTwitterCount()
   ]);
-
   console.log(
     `You have ${tCount} twitter followers and ${iCount} instagram followers!`
   );
-}
+  res.json({ iCount, tCount });
+});
 
-go();
+app.listen(2090, () => console.log(`App is running on port 2090`));
