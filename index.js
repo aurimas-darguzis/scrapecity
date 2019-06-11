7865fe8c828122a5217f05ff95d531fdf0ebfddf
Aurimas <aurimas@darguzis.com>
2019-06-11 09:28:14 +0100

    refactor

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-import {
-  getHTML,
-  getTwitterFollowers,
-  getInstagramFollowers
-} from './lib/scraper';
+import { getHTML, getInstagramCount, getTwitterCount } from './lib/scraper';
 
 async function go() {
-  const iPromise = getHTML('https://www.instagram.com/aurimasdarg');
-  const tPromise = getHTML('https://www.twitter.com/aurimasdarguzis');
-  const [instagramHTML, twitterHTML] = await Promise.all([iPromise, tPromise]);
-  const instagramCount = (await getInstagramFollowers(instagramHTML)) || 0;
-  const twCount = (await getTwitterFollowers(twitterHTML)) || 0;
+  const [iCount, tCount] = await Promise.all([
+    getInstagramCount(),
+    getTwitterCount()
+  ]);
+
   console.log(
-    `You have ${twCount} twitter followers and ${instagramCount} instagram followers!`
+    `You have ${tCount} twitter followers and ${iCount} instagram followers!`
   );
 }
 
